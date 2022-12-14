85011abc33f4e106bef3ff7c6ba2dbefce7893dc
lucialagenial <lucialagenial@gmail.com>
2022-12-14 16:34:50 +0100

    Wednesday's 15hrs updates

--- data_exploration.py
+++ data_exploration.py
@@ -70,7 +70,7 @@ print((dftmp.head(10)))
 
 fig, ax1 = plt.subplots(figsize=(10, 5))
 ax1.plot(dftmp.year.first(), dftmp.movieId.nunique(), "g-o")
-ax1.grid(None)
+#ax1.grid(None)
 ax1.set_ylim(0, )
 
 dftmp = ratings[['rating', 'timestamp']].groupby('timestamp')
@@ -208,3 +208,252 @@ print(rating_sum.T)
 rating_sum.sort_values(by='num_ratings_norm', inplace=True)
 plt.pie(rating_sum['num_ratings_norm'], labels=rating_sum.T.columns, labeldistance=2)
 plt.show()
+
+# 5. Average rating for all movies in each year, and also per genre.
+# INSIGHT: Slight decline in average movie ratings after 1960, but still remains above 3.
+# Range quite narrow, except for a few outliers.
+
+st = default_timer()
+
+plt.figure(figsize=(10, 5))
+# For each genre
+for genre in genres_unique.genre:
+    # Select only movies matching current genre. Pick only movieId (as index) and year.
+    dftmp = movies[movies[genre] == True][['movieId', 'year']].set_index('movieId')
+    # Selects ratings, with movieId as index. Select only movies that match also the previous step. Join both.
+    dftmp = dftmp.join(ratings[ratings.set_index('movieId').index.isin(dftmp.index)][['movieId', 'rating']]
+                       .groupby('movieId').mean())
+    # Now we have a df of movieId, year and multiple ratings. Group by year and compute the average rating.
+    dftmp = dftmp.groupby('year').mean()
+    plt.plot(dftmp, label=genre, alpha=0.7)
+
+# For all genres
+dftmp = movies[['movieId', 'year']].set_index('movieId')
+dftmp = dftmp.join(ratings[ratings.set_index('movieId').index.isin(dftmp.index)][['movieId', 'rating']]
+                   .groupby('movieId').mean())
+dftmp = dftmp.groupby('year').mean()
+plt.plot(dftmp, "o", label='All genres', color='black')
+
+plt.xlabel('Year')
+plt.ylabel('Rating')
+plt.title('Average rating per year per genre')
+plt.legend(loc=(1.05, 0), ncol=2)
+plt.ylim(0, )
+plt.figtext(0.5, 0.02, "There is a slight decline in average movie ratings after 1960, but still remains above 3."
+            , ha="center", fontsize=9, bbox={"facecolor": "white", "alpha": 0.5, "pad": 5})
+plt.show()
+
+# Plot 6:  Same as #5, but only the outliers now.
+# INSIGHT: All the outliers present vary few occurrences: IMAX 196, Film-Noir 330, no-genre 237.
+# In any case, Film-Noir movies are generally rated well above average, and the ones without any classification tend to
+# do very poorly...perhaps having no genre to anchor or relate to impairs critics?
+
+st = default_timer()
+
+plt.figure(figsize=(10, 5))
+for genre in outliers:
+    # Select only movies matching current genre. Pick only movieId (as index) and year.
+    dftmp = movies[movies[genre] == True][['movieId', 'year']].set_index('movieId')
+    # Selects ratings, with movieId as index. Select only movies that match also the previous step. Join both.
+    dftmp = dftmp.join(ratings[ratings.set_index('movieId').index.isin(dftmp.index)][['movieId', 'rating']]
+                       .groupby('movieId').mean())
+    # Now we have a df of movieId, year and multiple ratings. Group by year and compute the average rating.
+    dftmp = dftmp.groupby('year').mean()
+    plt.plot(dftmp, label=genre, alpha=0.7)
+
+# For all genres
+dftmp = movies[['movieId', 'year']].set_index('movieId')
+dftmp = dftmp.join(ratings[ratings.set_index('movieId').index.isin(dftmp.index)][['movieId', 'rating']]
+                   .groupby('movieId').mean())
+dftmp = dftmp.groupby('year').mean()
+plt.plot(dftmp, "o", label='All genres', color='black')
+
+plt.xlabel('Year')
+plt.ylabel('Rating')
+plt.title('Average rating per year (outlier genres)')
+plt.legend(loc=(1.05, 0), ncol=2)
+plt.ylim(0, )
+plt.show()
+
+# Plot 7: Avarage ratings per user:
+# INSIGHT#8**: Users have a positive bias in general, with roughly 95% of their average ratings above the mid-point of
+# 2.5. This is to be expected, and could have many explanations: users actually watch the better movies due to available
+# ratings (and this should get better over time, as the rating system expands); users don't bother that much to rate bad
+# movies as they do with the good ones (i.e. we don't want other to know our guilty pleasures... or disappoiments), etc.
+st = default_timer()
+
+dftmp = ratings[['userId', 'rating']].groupby('userId').mean()
+# Plot histogram
+dftmp.plot(kind='hist', bins=50, grid=0, normed=True, edgecolor='black', figsize=(10, 5))
+# Plot cumulative function on top (couldn't do any other way)
+# evaluate the histogram
+values, base = np.histogram(dftmp, bins=40, normed=True)
+# evaluate the cumulative (multiply by the average distance between points in the x-axis to get UNIT area)
+cumulative = np.cumsum(values) * np.diff(base).mean()
+# plot the cumulative function
+plt.plot(base[:-1], cumulative, c='blue', label='CDF')
+plt.xlim(0, 5)
+plt.legend()
+plt.xlabel('Average movie rating')
+plt.ylabel('Normalized frequency')
+plt.title('Average ratings per user')
+plt.show()
+
+# Plot 8: Rating timestamp vs. movie year vs. rating count
+# INSIGHT:  Besides the evident insight that newer movies get more ratings, and that older movies get a number of
+# ratings inversely proportional to their age, we can also see than the oldest movies have just received ratings very
+# recently (2010+), implying those were not readily for users to watch available before. There's also sort of a "block
+# behaviour", where at any point in time, movies with X number of ratings stop some Y years before (e.g. in 2005, there
+# are no movies older than 1980 with +10000 ratings).
+
+st = default_timer()
+# Combine movies year with ratings timestamps
+dftmp = movies[['movieId', 'year']].merge(ratings[['movieId', 'timestamp']], on='movieId')
+dftmp = dftmp.groupby(['year', 'timestamp']).count().reset_index()
+dftmp.columns = ['year', 'timestamp', 'count']
+dftmp = dftmp.pivot('year', 'timestamp')
+
+# Plot using a contour (color reflects number of ratings for each year-timestamp pair).
+# Could also use heatmap or imshow...
+X = dftmp.columns.levels[1].values
+Y = dftmp.index.values
+Z = dftmp.values
+Xi, Yi = np.meshgrid(X, Y)
+
+fig, axs = plt.subplots(2, 2)
+fig.set_size_inches(14, 7)
+cutoff = [np.nanmax(Z), 50000, 25000, 10000]  # Modify the contour plots a different max values.
+Zcap = np.copy(Z)
+locx = [0.45, 0.91, 0.45, 0.91]  # x location for the individual colorbars
+locy = [0.53, 0.53, 0.1, 0.1]  # y location for the individual colorbar
+for i, ax in enumerate(axs.flatten()):
+    Zcap[Zcap > cutoff[i]] = cutoff[i]
+    cmvals = ax.contourf(Yi, Xi, Zcap, 100, cmap=plt.cm.get_cmap("Spectral"))
+    ax.set_xlabel('Movie year')
+    ax.set_ylabel('Rating timestamp')
+    ax.set_title('Cutoff value: %s ratings' % cutoff[i], fontsize=10)
+    cax = fig.add_axes([locx[i], locy[i], 0.015, 0.35])
+    fig.colorbar(cmvals, cax=cax, orientation='vertical')
+fig.suptitle('Ratings per movie-year', fontsize=15)
+plt.subplots_adjust(hspace=0.35, wspace=0.45)
+
+# Highlight key regions in the plots, just roughly...
+axs.flatten()[2].plot([1980, 1995, 2015, 1998, 1980], [1998, 2014.5, 2014.5, 1998, 1998],
+                      '--', color='black', linewidth=3)
+axs.flatten()[3].plot([1970, 1985, 2015, 1998, 1970], [1998, 2014.5, 2014.5, 1998, 1998],
+                      '--', color='black', linewidth=3)
+plt.show()
+
+# Plot 9: Rating timestamp vs. movie year vs. average rating
+# INSIGHT: After 1980, average rating declines clearly; a lower ratings count for sure has an impact, but it seems
+# -perceived- movies quality did step-decline at that point. Also, movies released 1930-1980 and rated 1996-2003 seem to
+# conform the better rated block.
+
+st = default_timer()
+# Combine movies year with ratings timestamps
+dftmp = movies[['movieId', 'year']].merge(ratings[['movieId', 'timestamp', 'rating']], on='movieId')
+dftmp.drop('movieId', axis=1, inplace=True)
+dftmp = dftmp.groupby(['year', 'timestamp']).mean().reset_index().pivot('year', 'timestamp')
+
+# Plot using a contour (color reflects number of ratings for each year-timestamp pair).
+# Could also use heatmap or imshow...
+
+X = dftmp.columns.levels[1].values
+Y = dftmp.index.values
+Z = dftmp.values
+Xi, Yi = np.meshgrid(X, Y)
+
+fig, axs = plt.subplots(2, 2)
+fig.set_size_inches(14, 7)
+cutoff = [4.5, 4, 3.5, 3]  # Modify the contour plots a different max values.
+Zcap = np.copy(Z)
+locx = [0.45, 0.91, 0.45, 0.91]  # x location for the individual colorbars
+locy = [0.53, 0.53, 0.1, 0.1]  # y location for the individual colorbar
+for i, ax in enumerate(axs.flatten()):
+    Zcap[Zcap > cutoff[i]] = cutoff[i]
+    cmvals = ax.contourf(Yi, Xi, Zcap, 100, cmap=plt.cm.get_cmap("Spectral"))
+    ax.set_xlabel('Movie year')
+    ax.set_ylabel('Rating timestamp')
+    ax.set_title('Cutoff rating value: %s' % cutoff[i], fontsize=10)
+    cax = fig.add_axes([locx[i], locy[i], 0.015, 0.35])
+    fig.colorbar(cmvals, cax=cax, orientation='vertical')
+fig.suptitle('Average rating per movie-year', fontsize=15)
+plt.subplots_adjust(hspace=0.35, wspace=0.45)
+
+# Highlight key regions in the plots, just roughly...
+axs.flatten()[0].plot([1980, 1980, 2015, 1998, 1980], [1996, 2014.5, 2014.5, 1996, 1996],
+                      '--', color='black', linewidth=3)
+axs.flatten()[1].plot([1930, 1930, 1980, 1980, 1930], [1996, 2003, 2003, 1996, 1996],
+                      '--', color='black', linewidth=3)
+plt.show()
+
+# Plot 10: Ratings per user:
+# INSIGHT: As expected, exponentially declining function but with some outliers.
+
+st = default_timer()
+
+# Scatter plot of all users and individual ratings count.
+dftmp = ratings[['userId', 'movieId']].groupby('userId').count()
+dftmp.columns = ['num_ratings']
+plt.figure(figsize=(15, 5))
+plt.scatter(dftmp.index, dftmp.num_ratings, edgecolor='black')
+plt.xlim(0, len(dftmp.index))
+plt.ylim(0, )
+plt.title('Ratings per user')
+plt.xlabel('userId')
+plt.ylabel('Number of ratings given')
+plt.show()
+
+# Histogram of ratings counts.
+plt.figure(figsize=(15, 5))
+plt.hist(dftmp.num_ratings, bins=100, edgecolor='black', log=True)
+plt.title('Ratings per user')
+plt.xlabel('Number of ratings given')
+plt.ylabel('Number of userIds')
+plt.xlim(0, )
+plt.xticks(np.arange(0, 10000, 500))
+plt.show()
+
+# Plot 11: Ratings per movie
+# INSIGHT: The
+
+st = default_timer()
+
+# Scatter plot of all users and individual ratings count.
+dftmp = ratings[['userId', 'movieId']].groupby('movieId').count()
+dftmp.columns = ['num_ratings']
+plt.figure(figsize=(15, 5))
+plt.scatter(dftmp.index, dftmp.num_ratings, edgecolor='black')
+plt.xlim(0, dftmp.index.max())
+plt.ylim(0, )
+plt.title('Ratings per movie')
+plt.xlabel('movieId')
+plt.ylabel('Number of ratings received')
+plt.show()
+
+# Histogram of ratings counts.
+plt.figure(figsize=(15, 5))
+plt.hist(dftmp.num_ratings, bins=100, edgecolor='black', log=True)
+plt.title('Ratings per movie')
+plt.xlabel('Number of ratings received')
+plt.ylabel('Number of movieIds')
+plt.xlim(0, )
+plt.show()
+
+# Let's check those movies with +40k reviews, those should be pretty popular movies!
+print(movies.set_index('movieId').loc[dftmp.index[dftmp.num_ratings > 150]]['title'])
+
+print(ratings.groupby('rating')['movieId'].mean().round().sort_values(ascending=False))
+# Let's check the average rating too, those should be pretty good movies!
+ratings.set_index('movieId').loc[dftmp.index[dftmp.num_ratings > 150]].groupby('movieId').mean().rating.plot(style='o')
+plt.ylabel('Average rating')
+plt.title('Most popular movies rating')
+plt.show()
+
+# Which is the best most popular movie ever??
+tmp = ratings.set_index('movieId').loc[dftmp.index[dftmp.num_ratings > 150]].groupby('movieId').mean()
+best = movies.set_index('movieId').loc[tmp.rating.idxmax].title
+print(f'Best most popular movie ever is: {best}')
+
+
+# Source: https://www.kaggle.com/code/cesarcf1977/movielens-data-analysis-beginner-s-first/notebook
---
+++ model_recommender_kNN.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+
+
+# Movie Recommendation based on K-Nearest-Neighbors (KNN)
+
+# We'll try to guess the rating of a movie by looking at the 10 movies that are closest to it in terms of genres and
+# popularity.
+
+# Load the data
+movies = pd.read_csv("ml-latest-small/movies.csv")
+ratings = pd.read_csv("ml-latest-small/ratings.csv")
+tags = pd.read_csv("ml-latest-small/tags.csv")
+
+# Group the movie ID, and compute the total number of ratings and the average for every movie.
+movieProperties = ratings.groupby('movieId').agg({'rating': [np.size, np.mean]})
+print(movieProperties.head())
+
+# New df that contains the normalized number of ratings. So, a value of 0 means nobody rated it, and a value of 1 will
+# mean it's the most popular movie there is.
+
+movieNumRatings = pd.DataFrame(movieProperties['rating']['size'])
+movieNormalizedNumRatings = movieNumRatings.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+print(movieNormalizedNumRatings.head())
+
+# Add the genre information:
+# The way this works is there are 19 fields, each corresponding to a specific genre - a value of '0' means it is not in
+# that genre, and '1' means it is in that genre. A movie may have more than one genre associated with it.
+
+# While we're at it, we'll put together everything into one big Python dictionary called movieDict. Each entry will
+# contain the movie name, list of genre values, the normalized popularity score, and the average rating for each movie:
+
+movieDict = {}
+with open(r'ml-latest-small/movies.csv', encoding="ISO-8859-1") as f:
+    temp = ''
+    for line in f:
+        #line.encode().decode("ISO-8859-1")
+        fields = line.rstrip('\n').split('|')
+        movieID = int()
+        name = fields[1]
+        genres = fields[5:25]
+        genres = map(int, genres)
+        movieDict[movieID] = (name, np.array(list(genres)), movieNormalizedNumRatings.loc[movieID].get('size'),
+                              movieProperties.loc[movieID].rating.get('mean'))
+
+
+# For example, here's the record we end up with for movie ID 1, "Toy Story":)
+
+print(movieDict[1])
+
+
+
+# Source: https://hendra-herviawan.github.io/Movie-Recommendation-based-on-KNN-K-Nearest-Neighbors.html
